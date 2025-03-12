d908f5f5f8ccafe95e1c361d8fc8e87d5e6d95d8
Dr. Wang <37176118+wangyong950511@users.noreply.github.com>
2025-03-12 08:38:26 +0800

    Update RNAseq_getdata.R

--- 结构化学习/RNAseq_getdata.R
+++ 结构化学习/RNAseq_getdata.R
@@ -21,11 +21,14 @@ countData <- read.table("~/R/RNA/data/NASH/GSE287943/GSE287943_countData.txt.gz"
 ### 变种1-读取xlsx文件
 count_data <- read_excel("~/R/RNA/data/NASH/GSE253217/GSE253217_Liver_tmm_normalized_counts.xlsx")
 ### 变种1-读取tsv文件
-df <- read_tsv("~/R/RNA/data/NASH/GSE236832/GSE236832_de_summary.tsv.gz", skip = 1)
+df <- read_tsv("~/R/RNA/data/NASH/GSE236832/GSE236832_de_summary.tsv.gz")
 ### 变种1-读取csv文件
 countData <- read.csv(gzfile("~/R/RNA/data/NASH/GSE230745/GSE230745_kidney_chp_raw_counts.csv.gz"), header = TRUE, row.names = 1)
 ### 万能解决
 countData <- read.delim(gzfile("~/R/RNA/data/NASH/GSE275069/GSM8465145_Matrix_HF_vs_ND.txt.gz"),header = TRUE,sep = "\t",fill = TRUE)
+### 自己跑上游
+rawData <- read.table("~/R/RNA/data/NASH/GSE160292/gene_counts.txt", skip=0,header = TRUE, row.names = 1)
+countData <- rawData[, -(1:5)]
 
 
 
