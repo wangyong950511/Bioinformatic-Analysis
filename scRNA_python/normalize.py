f57660e340bc07a383c8df009ac49febe98cc914
Dr. Wang <37176118+wangyong950511@users.noreply.github.com>
2025-07-30 23:27:56 +0800

    Create normalize.py

---
+++ scRNA_python/normalize.py
@@ -0,0 +1,7 @@
+
+
+
+sc.pp.neighbors(adata_CD8T, use_rep="X_pca_harmony")
+sc.tl.umap(adata_CD8T)
+sc.tl.leiden(adata_CD8T, resolution=0.5, key_added="leiden_CD8T_0.5")
+sc.pl.umap(adata_CD8T, color=["leiden_CD8T_0.5"])
